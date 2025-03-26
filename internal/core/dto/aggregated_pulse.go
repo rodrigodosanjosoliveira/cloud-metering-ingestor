3a7933a4f0b688dbf2ce5a3c1c60eb4057b9ba94
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2025-03-26 18:32:22 -0300

    refactor(dto): extract AggregatedPulseDTO to dedicated core DTO package

--- internal/core/dto/aggregated_pulse.go
+++ internal/core/dto/aggregated_pulse.go
@@ -1,1 +1,8 @@
 package dto
+
+type AggregatedPulse struct {
+	Tenant     string  `json:"tenant"`
+	ProductSKU string  `json:"product_sku"`
+	UseUnit    string  `json:"use_unit"`
+	TotalUsed  float64 `json:"total_used"`
+}
